aa62a6caae4f0693483b51fc19224460c273d873
boris-win <paur.link@gmail.com>
2014-12-22 01:27:52 +0200

    Cleanup tests

--- Tests/Libs/ApplicationServiceModel/IoCTest.cs
+++ Tests/Libs/ApplicationServiceModel/IoCTest.cs
@@ -1,5 +1,4 @@
-﻿using MMK.Application;
-using MMK.ApplicationServiceModel.Locator;
+﻿using MMK.ApplicationServiceModel.Locator;
 using MMK.ApplicationServiceModel.Locator.Resolving;
 using NUnit.Framework;
 
@@ -9,38 +8,10 @@ namespace MMK.ApplicationServiceModel
     public class IoCTest
     {
         [Test]
-        [ExpectedException(typeof(ServiceLocatorConflictException))]
+        [ExpectedException(typeof(ServiceLocatorNotFoundException))]
         public void ThrowsServiceLocatorNotFoundException()
         {
-            IoC.Get<object>();
-        }
-    }
-
-    [ServiceLocatorOwner]
-    public class DummyServiceLocatorOwner
-    {
-
-        [ServiceLocator]
-        public static object ServiceLocator
-        {
-            get
-            {
-                return new object();
-            }
-        }
-    }
-
-    [ServiceLocatorOwner]
-    public class DummyServiceLocatorOwner2
-    {
-
-        [ServiceLocator]
-        public static object ServiceLocator
-        {
-            get
-            {
-                return new object();
-            }
+            IoC.ServiceLocator.Get<object>();
         }
     }
 }
