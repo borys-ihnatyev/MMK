ebc66a9789ae28c8605ae8639c5526b5d98236e1
boris-win <paur.link@gmail.com>
2014-12-21 20:13:06 +0200

    Refactor Project structure Move Tray icon login in Controller

--- Src/Apps/Tray/Notify/App.xaml.cs
+++ Src/Apps/Tray/Notify/App.xaml.cs
@@ -4,30 +4,26 @@ using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Forms;
+using MMK.Notify.Controllers;
 using MMK.Notify.Model;
 using MMK.Notify.Model.Launchers;
 using MMK.Notify.Observer;
 using MMK.Notify.Observer.Remoting;
 using MMK.Notify.Observer.Tasking.Observing;
 using MMK.Notify.Properties;
-using MMK.Notify.View.TrayMenu;
-using MMK.Notify.ViewModel.TrayMenu;
+using MMK.Notify.Views.TrayMenu;
 using MMK.Processing.AutoFolder;
 using MMK.Wpf.Providers;
 using Application = System.Windows.Application;
-using Timer = System.Timers.Timer;
 
 namespace MMK.Notify
 {
     public partial class App : INotifyObserverOwner
     {
-        private static TrayMenuViewModel viewModel;
-
-        private readonly NotifyIcon notifyIcon;
-
         private readonly TaskObserver taskObserver;
         private readonly NotifyObserver notifyObserver;
         private readonly NotificationController notification;
+        private readonly TrayMenuController trayMenuController;
 
         private readonly GlobalShortcutProviderCollection shortcutProviders;
 
@@ -67,19 +63,19 @@ namespace MMK.Notify
                 Shutdown();
 
             taskObserver = new TaskObserver();
+            taskObserver.TaskObserved += TaskObserved;
+
             notifyObserver = new NotifyObserver(taskObserver);
 
             notification = new NotificationController();
 
             MusicDownloadsWatcher = new MusicDownloadsWatcher();
-            notifyIcon = new NotifyIcon
-            {
-                Icon = Notify.Properties.Resources.NotifyLogo,
-                Text = @"MMK Notify",
-                Visible = true
-            };
+            MusicDownloadsWatcher.FileDownloaded += notifyObserver.NormalizeTrackName;
 
             shortcutProviders = new GlobalShortcutProviderCollection();
+
+            trayMenuController = new TrayMenuController();
+            trayMenuController.WindowInitialize += OnWindowInitialize;
         }
 
         public HashTagFolderCollection FolderCollection { get; set; }
@@ -104,37 +100,34 @@ namespace MMK.Notify
         protected override void OnStartup(StartupEventArgs e)
         {
             Initialize();
-
-            MainWindow = new TrayMenuWindow(ViewModel);
-
-            if (MainWindow.IsLoaded)
-            {
-                LoadShortcuts();
-                ViewModel.LoadData();
-            }
-            else
-            {
-                MainWindow.Loaded += (sender, args) => LoadShortcuts();
-                MainWindow.Loaded += (sender, args) => ViewModel.LoadData();
-            }
-
-            MainWindow.Show();
+            StartServices();
         }
 
         private void Initialize()
         {
-            taskObserver.TaskObserved += TaskObserved;
-            MusicDownloadsWatcher.FileDownloaded += notifyObserver.NormalizeTrackName;
-
-            notifyIcon.Click += NotifyIcon_Click;
-            notifyIcon.Visible = true;
-
-            StartNotifyObserver();
-            taskObserver.Start();
-
+            trayMenuController.Initialize();
             InitializeFolderCollection();
         }
 
+        private void OnWindowInitialize(object sender, EventArgs<Window> e)
+        {
+            MainWindow = e.Arg;
+            MainWindow.Loaded += (s, a) => LoadShortcuts();
+        }
+
+        private void InitializeFolderCollection()
+        {
+            FolderCollection = Settings.Default.FolderCollection;
+        }
+
+        private void StartServices()
+        {
+            taskObserver.Start();
+            StartNotifyObserver();
+
+            MainWindow = trayMenuController.TrayMenuWindow;
+            trayMenuController.Start();
+        }
 
         private void StartNotifyObserver()
         {
@@ -146,52 +139,17 @@ namespace MMK.Notify
 #endif
         }
 
-        private void InitializeFolderCollection()
-        {
-            FolderCollection = Settings.Default.FolderCollection;
-        }
-
         public new static App Current
         {
             get { return (App) Application.Current; }
         }
 
-        public static TrayMenuViewModel ViewModel
-        {
-            get { return viewModel ?? (viewModel = new TrayMenuViewModel()); }
-        }
-
         #region Behaviors
-
-        protected override void OnDeactivated(EventArgs e)
-        {
-            ViewModel.IsVisible = false;
-            notifyIcon.Click -= NotifyIcon_Click;
-
-            var timer = new Timer(210);
-
-            timer.Elapsed += (s, a) =>
-            {
-                notifyIcon.Click += NotifyIcon_Click;
-                timer.Stop();
-                timer.Dispose();
-            };
-
-            timer.Start();
-
-            base.OnDeactivated(e);
-        }
 
         protected override void OnExit(ExitEventArgs e)
         {
             taskObserver.Dispose();
             base.OnExit(e);
-        }
-
-        private void NotifyIcon_Click(object sender, EventArgs e)
-        {
-            ViewModel.IsVisible = true;
-            MainWindow.Activate();
         }
 
         #endregion
---
+++ Src/Apps/Tray/Notify/Controllers/TrayMenuController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using MMK.Notify.Properties;
+using MMK.Notify.ViewModels.TrayMenu;
+using MMK.Notify.Views.TrayMenu;
+using Application = System.Windows.Application;
+
+namespace MMK.Notify.Controllers
+{
+    public class TrayMenuController
+    {
+        private bool initialized;
+        private TrayMenuWindow trayMenuWindow;
+        private readonly TrayMenuViewModel trayMenuViewModel;
+        private readonly NotifyIcon trayIcon;
+
+        public event EventHandler<EventArgs<Window>> WindowInitialize;
+
+        public TrayMenuController()
+        {
+            trayIcon = new NotifyIcon
+            {
+                Icon = Resources.NotifyLogo,
+                Text = @"MMK Notify"
+            };
+
+            trayMenuViewModel = new TrayMenuViewModel();
+        }
+
+        public Window TrayMenuWindow
+        {
+            get { return trayMenuWindow; }
+        }
+
+        public void Initialize()
+        {
+            if (initialized)
+                return;
+            InitializeTrayIcon();
+            InitializeTrayWindow();
+            initialized = true;
+        }
+
+        private void InitializeTrayWindow()
+        {
+            trayMenuWindow = new TrayMenuWindow();
+            trayMenuWindow.BeginInit();
+            
+            OnWindowInitialize();
+
+            trayMenuWindow.DataContext = trayMenuViewModel;
+            trayMenuWindow.Loaded += (sender, args) => trayMenuViewModel.LoadData();
+        }
+
+        protected virtual void OnWindowInitialize()
+        {
+            var handler = WindowInitialize;
+            if (handler != null) 
+                handler(this, new EventArgs<Window>(trayMenuWindow));
+        }
+
+        private void InitializeTrayIcon()
+        {
+            trayIcon.Click += OnTrayIconClick;
+            trayIcon.Visible = true;
+            Application.Current.Deactivated += OnAppDeactivated;
+        }
+
+        private void OnTrayIconClick(object sender, EventArgs eventArgs)
+        {
+            trayMenuViewModel.IsVisible = true;
+            trayMenuWindow.Activate();
+        }
+
+        private void OnAppDeactivated(object sender, EventArgs eventArgs)
+        {
+            trayMenuViewModel.IsVisible = false;
+            trayIcon.Click -= OnTrayIconClick;
+
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(210),
+                IsEnabled = false
+            };
+
+            timer.Tick += (s, a) =>
+            {
+                trayIcon.Click += OnTrayIconClick;
+                timer.Stop();
+            };
+
+            timer.Start();
+        }
+
+        public void Start()
+        {
+            if (!initialized)
+                throw new Exception("instanse was not Initialized");
+
+            trayMenuWindow.EndInit();
+            trayIcon.Visible = true;
+            trayMenuWindow.Show();
+        }
+    }
+}
