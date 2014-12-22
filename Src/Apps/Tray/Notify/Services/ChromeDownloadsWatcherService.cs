9c29855929be6583a779726a6bbb96e4d295a80b
boris-win <paur.link@gmail.com>
2014-12-22 09:15:22 +0200

    Move downloads watcher into service

--- Src/Apps/Tray/Notify/Services/ChromeDownloadsWatcherService.cs
+++ Src/Apps/Tray/Notify/Services/ChromeDownloadsWatcherService.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.IO;
+using MMK.ApplicationServiceModel;
 using MMK.Utils;
 
-namespace MMK.Notify.Model
+namespace MMK.Notify.Services
 {
-    /// <summary>
-    ///     TODO : provide for each browser specific (now is only for CHROME)
-    /// </summary>
-    public sealed class MusicDownloadsWatcher
+    public sealed class ChromeDownloadsWatcherService : Service
     {
-        private static readonly string DefaultDownloadsPath = Environment.ExpandEnvironmentVariables("%USERPROFILE%") +
-                                                              @"\Downloads\";
+        private static readonly string DefaultDownloadsPath;
 
-        public MusicDownloadsWatcher()
+        static ChromeDownloadsWatcherService()
+        {
+            DefaultDownloadsPath = Environment
+                .ExpandEnvironmentVariables("%USERPROFILE%") + @"\Downloads\";
+        }
+
+        public ChromeDownloadsWatcherService()
         {
             fileWatcher = new FileSystemWatcher(DefaultDownloadsPath);
             fileWatcher.Renamed += OnRenamed;
@@ -32,12 +35,12 @@ namespace MMK.Notify.Model
                 FileDownloaded(filePath);
         }
 
-        public void Start()
+        public override void Start()
         {
             fileWatcher.EnableRaisingEvents = true;
         }
 
-        public void Stop()
+        public override void Stop()
         {
             fileWatcher.EnableRaisingEvents = false;
         }
