ae1bb1f0480c31511fc100bb4dd87f34e0236e03
boris-win <paur.link@gmail.com>
2014-12-25 04:18:56 +0200

    Add Player Usage Project

---
+++ Usages/HotMarkMediaPlayer/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using MMK.HotMark.MediaPlayer.Views;
+using MMK.HotMark.ViewModels;
+
+namespace MMK.HotMark.MediaPlayer
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            StartupMainView();
+
+            base.OnStartup(e);
+        }
+
+        private const string StartupFile = @"D:\Azari, Fritz Helder, Iii, Tesla Boy - PARAFFIN #emoll #mix@art-cafe.mp3";
+
+        private void StartupMainView()
+        {
+            MainWindow = new HotMarkPlayerView(new PlayerViewModel(StartupFile));
+            MainWindow.Show();
+        }
+    }
+}
