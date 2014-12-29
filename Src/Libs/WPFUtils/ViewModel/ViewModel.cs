4262b022884c14a09b5b9ad999e743163c133f23
boris-win <paur.link@gmail.com>
2014-12-29 23:39:53 +0200

    Extract ViewModelsLoader in class

--- Src/Libs/WPFUtils/ViewModel/ViewModel.cs
+++ Src/Libs/WPFUtils/ViewModel/ViewModel.cs
@@ -1,61 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace MMK.Wpf.ViewModel
 {
     public abstract class ViewModel : ObservableObject, IDataErrorInfo
     {
+        private readonly ViewModelLoader viewModelsLoader;
+
+        protected ViewModel()
+        {
+            viewModelsLoader = new ViewModelLoader(this);
+        }
+
         public bool IsDataLoaded { get; set; }
+
 
         public void LoadData()
         {
-            if(IsDataLoaded) return;
+            if (IsDataLoaded) return;
 
             OnLoadData();
-            LoadDeclaredViewModels();
+            viewModelsLoader.Load();
             IsDataLoaded = true;
-        }
-
-        private IEnumerable<ViewModel> GetDeclaredViewModels()
-        {
-            return GetType()
-                .GetProperties()
-                .Where(prop => prop.PropertyType.IsSubclassOf(typeof (ViewModel)))
-                .Select(prop => prop.GetMethod.Invoke(this, null))
-                .OfType<ViewModel>();
         }
 
         protected virtual void OnLoadData()
         {
-            
         }
 
-        private void LoadDeclaredViewModels()
-        {
-            GetDeclaredViewModels().ForEach(vm => vm.LoadData());
-        }
 
         public void UnloadData()
         {
-            if(!IsDataLoaded) return;
+            if (!IsDataLoaded) return;
 
             OnUnloadData();
-            UnloadDeclaredViewModels();
+            viewModelsLoader.Unload();
+
             IsDataLoaded = false;
         }
 
         protected virtual void OnUnloadData()
         {
-
-        }
-
-        private void UnloadDeclaredViewModels()
-        {
-            GetDeclaredViewModels().ForEach(vm => vm.UnloadData());
         }
 
         #region IDataErrorInfo
@@ -68,20 +55,17 @@ namespace MMK.Wpf.ViewModel
         protected virtual string OnValidation(string columnName)
         {
             var errors = new Collection<ValidationResult>();
-            
+
             var validationContext = new ValidationContext(this);
-            
-            return Validator.TryValidateProperty(columnName, validationContext, errors) 
-                ? null 
+
+            return Validator.TryValidateProperty(columnName, validationContext, errors)
+                ? null
                 : errors[0].ErrorMessage;
         }
 
         public string Error
         {
-            get
-            {
-                throw new NotSupportedException();
-            }
+            get { throw new NotSupportedException(); }
         }
 
         #endregion
