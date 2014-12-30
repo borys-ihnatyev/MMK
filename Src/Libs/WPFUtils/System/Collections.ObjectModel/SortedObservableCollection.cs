82216f3463e6681266479c507923148d5cd23591
boris-win <paur.link@gmail.com>
2014-12-30 19:22:11 +0200

    Remove Unused constructor

--- Src/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollection.cs
+++ Src/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollection.cs
@@ -1,66 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.Contracts;
 
 namespace System.Collections.ObjectModel
 {
-    public class SortedObservableCollection<T> : ObservableCollection<T>
+    public class SortedObservableCollection<T> : ObservableCollection<T> where T : INotifyPropertyChanged
     {
+        private readonly IComparer<T> comparer;
+
+        #region ctors
+
         public SortedObservableCollection(IComparer<T> comparer)
         {
+            if (comparer == null)
+                throw new ArgumentNullException("comparer");
+            Contract.EndContractBlock();
+
             this.comparer = comparer;
         }
 
         public SortedObservableCollection(IEnumerable<T> collection, IComparer<T> comparer)
-            : base(collection)
         {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            if (comparer == null)
+                throw new ArgumentNullException("comparer");
+            Contract.EndContractBlock();
+
             this.comparer = comparer;
+
+            foreach (var item in collection)
+                Add(item);
         }
 
-        public SortedObservableCollection(List<T> list, IComparer<T> comparer)
-            : base(list)
-        {
-            this.comparer = comparer;
-        }
-
-        #region functional
-        private readonly IComparer<T> comparer;
-
-        private int GetItemIndex(T item)
-        {
-            if (Count == 0)
-                return 0;
-
-            for (int i = Count - 1; i > -1; i--)
-            {
-                int compareResult = comparer.Compare(item, this[i]);
-                if (compareResult == 1 || compareResult == 0)
-                    return i + 1;
-            }
-            return 0;
-        }
-
-        private void InsertItem(T item) 
-        {
-            if (item is INotifyPropertyChanged) 
-            {
-                var changeableItem = (INotifyPropertyChanged)item;
-                changeableItem.PropertyChanged += OnItemChanged;
-            }
-            base.InsertItem(GetItemIndex(item), item);
-        }
-
-        private void OnItemChanged(object sender, PropertyChangedEventArgs e)
-        {
-            int itemIndex = IndexOf((T)sender);
-            UpdateItem(itemIndex);
-        }
-
-        private void UpdateItem(int index) 
-        {
-            var item = this[index];
-            RemoveItem(index);
-            InsertItem(item);
-        }
         #endregion
 
         protected override void InsertItem(int index, T item)
@@ -68,13 +40,43 @@ namespace System.Collections.ObjectModel
             InsertItem(item);
         }
 
+        private void InsertItem(T item)
+        {
+            item.PropertyChanged += OnItemChanged;
+            base.InsertItem(GetItemIndex(item), item);
+        }
+
+        private int GetItemIndex(T item)
+        {
+            if (Count == 0)
+                return 0;
+
+            for (var i = Count - 1; i > -1; i--)
+            {
+                var compareResult = comparer.Compare(item, this[i]);
+                if (compareResult == 1 || compareResult == 0)
+                    return i + 1;
+            }
+            return 0;
+        }
+
         protected override void RemoveItem(int index)
         {
-            var item = this[index] as INotifyPropertyChanged;
-            if(item != null)
-                item.PropertyChanged -= OnItemChanged;
-              
+            this[index].PropertyChanged -= OnItemChanged;
             base.RemoveItem(index);
+        }
+
+        private void OnItemChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var itemIndex = IndexOf((T) sender);
+            UpdateItem(itemIndex);
+        }
+
+        protected virtual void UpdateItem(int index)
+        {
+            var item = this[index];
+            RemoveItem(index);
+            InsertItem(item);
         }
 
         protected override void SetItem(int index, T item)
