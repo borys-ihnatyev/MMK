d63c01bd9e893256b815b411ee858079f2dcabf1
boris-win <paur.link@gmail.com>
2014-12-31 19:08:41 +0200

    Finish basic SortedObservable collection functional

--- Src/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollection.cs
+++ Src/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollection.cs
@@ -35,32 +35,91 @@ namespace System.Collections.ObjectModel
 
         #endregion
 
-        protected override void InsertItem(int index, T item)
+        protected override sealed void InsertItem(int index, T item)
         {
-            InsertItem(item);
-        }
-
-        private void InsertItem(T item)
-        {
+            item.PropertyChanged -= OnItemChanged;
             item.PropertyChanged += OnItemChanged;
-            base.InsertItem(GetItemIndex(item), item);
+            base.InsertItem(GetInsertItemIndex(item), item);
         }
 
-        private int GetItemIndex(T item)
+        /// <summary>
+        /// For items added in collection
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private int GetInsertItemIndex(T item)
         {
             if (Count == 0)
                 return 0;
+            
+            for (var i = 0; i < Count; i++)
+            {
+                if(comparer.Compare(item,Items[i]) == 1)
+                    continue;
+                return i;
+            }
 
-            for (var i = Count - 1; i > -1; i--)
+            return Count;
+        }
+
+        /// <summary>
+        /// For elements that still in collection
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private int GetReplaceItemIndex(T item, int index)
+        {
+            var prevIndex = index - 1;
+            var nextIndex = index + 1;
+
+            if (prevIndex == -1 && nextIndex == Count)
+                return index;
+
+            if (prevIndex == -1)
+                return GetIndexIteratingForward(item, index);
+
+            if (nextIndex == Count)
+                return GetIndexIteratingBackward(item, index);
+
+            var isIterFor = comparer.Compare(item, this[nextIndex]) == 1;
+            var isIterBack = comparer.Compare(this[prevIndex], item) == 1;
+
+            if (isIterFor && !isIterBack)
+                return GetIndexIteratingForward(item, index);
+            
+            if (isIterBack && !isIterFor)
+                return GetIndexIteratingBackward(item, index);
+
+            Contract.Assert(!(isIterBack && isIterFor));
+
+            return index;
+        }
+
+        private int GetIndexIteratingForward(T item, int index)
+        {
+            for (var i = index + 1; i < Count; ++i)
             {
                 var compareResult = comparer.Compare(item, this[i]);
-                if (compareResult == 1 || compareResult == 0)
+                if (compareResult == -1)
+                    return i - 1;
+            }
+
+            return Count - 1;
+        }
+
+        private int GetIndexIteratingBackward(T item, int index)
+        {
+            for (var i = index - 1; i > -1; --i)
+            {
+                var compareResult = comparer.Compare(item, this[i]);
+                if (compareResult == 1)
                     return i + 1;
             }
             return 0;
         }
 
-        protected override void RemoveItem(int index)
+        sealed protected override void RemoveItem(int index)
         {
             this[index].PropertyChanged -= OnItemChanged;
             base.RemoveItem(index);
@@ -75,14 +134,17 @@ namespace System.Collections.ObjectModel
         protected virtual void UpdateItem(int index)
         {
             var item = this[index];
-            RemoveItem(index);
-            InsertItem(item);
+            base.MoveItem(index, GetReplaceItemIndex(item, index));
         }
 
-        protected override void SetItem(int index, T item)
+        sealed protected override void SetItem(int index, T item)
         {
-            RemoveItem(index);
-            InsertItem(item);
+            throw new InvalidOperationException("You can't replace items by index property");
+        }
+
+        sealed protected override void MoveItem(int oldIndex, int newIndex)
+        {
+            throw new InvalidOperationException("Can't manualy move items in sorted collection");
         }
     }
 }
--- Tests/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollectionTest.cs
+++ Tests/Libs/WPFUtils/System/Collections.ObjectModel/SortedObservableCollectionTest.cs
@@ -42,9 +42,7 @@ namespace System.Collections.ObjectModel
             var intEnumerable = CreateTestEnumerable();
             collection = new SortedObservableCollection<ObservbleInt32>(intEnumerable, new ObservbleInt32.Comparer());
 
-            Assert.AreEqual(0, collection[0]);
-            Assert.AreEqual(1, collection[1]);
-            Assert.AreEqual(2, collection[2]);
+            AssertCollectionIsSorted();
         }
 
         private static IEnumerable<ObservbleInt32> CreateTestEnumerable()
@@ -73,12 +71,11 @@ namespace System.Collections.ObjectModel
             collection.Add(100);
             collection.Add(0);
 
-            Assert.AreEqual(0, collection[0]);
-            Assert.AreEqual(100, collection[1]);
+            AssertCollectionIsSorted();
         }
 
         [Test]
-        public void ResortCollection_When_ItemPropertyChanged()
+        public void ResortCollectionOf3_When_FirstItemPropertyChanged()
         {
             var observableInt32 = new ObservbleInt32(0);
             collection.Add(observableInt32);
@@ -87,23 +84,115 @@ namespace System.Collections.ObjectModel
 
             observableInt32.Number = 200;
 
-            Assert.AreEqual(100, collection[0]);
-            Assert.AreEqual(200, collection[1]);
-            Assert.AreEqual(300, collection[2]);
+            AssertCollectionIsSorted();
         }
 
         [Test]
-        public void ResortCollection_When_ItemChanged()
+        public void ResortCollectionOf3_When_LastItemPropertyChanged()
+        {
+            collection.Add(100);
+            collection.Add(300);
+            var observableInt32 = new ObservbleInt32(400);
+            collection.Add(observableInt32);
+
+            observableInt32.Number = 200;
+
+            AssertCollectionIsSorted();
+        }
+
+        [Test]
+        public void ResortCollectionOf3_When_MiddleItemPropertyChanged_WithHigh()
+        {
+            collection.Add(100);
+            var observableInt32 = new ObservbleInt32(200);
+            collection.Add(observableInt32);
+            collection.Add(300);
+
+            observableInt32.Number = 400;
+
+            AssertCollectionIsSorted();
+        }
+
+        [Test]
+        public void ResortCollectionOf3_When_MiddleItemPropertyChanged_WithLow()
+        {
+            collection.Add(100);
+            var observableInt32 = new ObservbleInt32(200);
+            collection.Add(observableInt32);
+            collection.Add(300);
+
+            observableInt32.Number = 50;
+
+            AssertCollectionIsSorted();
+        }
+
+        [Test]
+        public void ResortCollectionOf3_When_MiddleItemPropertyChanged()
+        {
+            collection.Add(100);
+            var observableInt32 = new ObservbleInt32(200);
+            collection.Add(observableInt32);
+            collection.Add(300);
+
+            observableInt32.Number = 250;
+
+            AssertCollectionIsSorted();
+        }
+
+        [Test]
+        public void ResortCollectionOf1_When_ItemPropertyChanged()
+        {
+            var observableInt32 = new ObservbleInt32(0);
+            collection.Add(observableInt32);
+
+            observableInt32.Number = 200;
+
+            Assert.AreEqual(200, collection[0]);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ThrowsInvalidOperatioException_When_AccessIndexerSetter()
         {
             collection.Add(100);
             collection.Add(200);
             collection.Add(300);
 
             collection[0] = 400;
+        }
 
-            Assert.AreEqual(200, collection[0]);
-            Assert.AreEqual(300, collection[1]);
-            Assert.AreEqual(400, collection[2]);
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ThrowsInvalidOperatioException_When_Move()
+        {
+            collection.Add(100);    
+            collection.Add(200);
+            collection.Add(300);
+
+            collection.Move(0,1);
+        }
+
+        [Test]
+        public void WhenRemove_CountDecreased()
+        {
+            var observableInt32 = new ObservbleInt32(0);
+            collection.Add(observableInt32);
+            collection.Add(100);
+            collection.Add(300);
+
+            var initialCount = collection.Count;
+
+            Assert.True(collection.Remove(observableInt32));
+            Assert.AreEqual(initialCount - 1, collection.Count);
+        }
+
+        private void AssertCollectionIsSorted()
+        {
+            var array = collection.ToArray();
+            Array.Sort(array, new ObservbleInt32.Comparer());
+
+            for (var i = 0; i < array.Length; i++)
+                Assert.AreEqual(array[i],collection[i]);
         }
     }
 }
