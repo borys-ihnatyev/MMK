989adf3c6dda1fe2a7927cfe383f34c29356d83b
Boris <paur.link@gmail.com>
2015-01-17 01:26:55 +0200

    fix search

--- Src/Apps/SwiftSearch/Views/SwiftSearchView.xaml.cs
+++ Src/Apps/SwiftSearch/Views/SwiftSearchView.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using MMK.SwiftSearch.ViewModels;
 
 namespace MMK.SwiftSearch.Views
 {
     public partial class SwiftSearchView
     {
+        private readonly SwiftSearchViewModel viewModel;
+
         public SwiftSearchView(SwiftSearchViewModel viewModel)
         {
             InitializeComponent();
-
+            this.viewModel = viewModel;
+            
             DataContext = viewModel;
+
             viewModel.SearchSelfChanged += OnSearchChangedByViewModel;
             Loaded += (s, e) => viewModel.LoadData();
             Loaded += (s, e) => SearchTextBox.Focus();
@@ -30,6 +35,16 @@ namespace MMK.SwiftSearch.Views
         {
             if (e.WidthChanged)
                 Left = SystemParameters.WorkArea.Left + (SystemParameters.WorkArea.Width - e.NewSize.Width)/2;
+        }
+
+        private void OnCloseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var applySearch = (bool) e.Parameter;
+            
+            if(applySearch)
+                viewModel.ApplySearch();
+            
+            Close();
         }
     }
 }
