09a2e5afc4ee528dfef42e275175faa75c9b32d9
Boris <paur.link@gmail.com>
2015-01-21 01:36:47 +0200

    Fix settings bad read

--- Src/Apps/Tray/Notify/ViewModels/TrayMenuViewModel.cs
+++ Src/Apps/Tray/Notify/ViewModels/TrayMenuViewModel.cs
@@ -17,9 +17,6 @@ namespace MMK.Notify.ViewModels
     {
         private Window hashTagFoldersWindow;
 
-        private bool isEnableHotKeys;
-        private bool isEnableDownloadsWatch;
-
         public TrayMenuViewModel()
         {
             StartListenShortcutsCommand = new Command(IoC.Get<GlobalShortcutService>().Start);
@@ -27,32 +24,28 @@ namespace MMK.Notify.ViewModels
 
             StartDownloadsWatchCommand = new Command(IoC.Get<IDownloadsWatcher>().Start);
             StopDownloadsWatchCommand = new Command(IoC.Get<IDownloadsWatcher>().Stop);
-
-            PropertyChanged += OnPropertyChanged;
         }
 
-        [SettingsProperty]
         public bool IsEnableHotKeys
         {
-            get { return isEnableHotKeys; }
+            get { return Settings.Default.IsEnableHotKeys; }
             set
             {
-                if (value == isEnableHotKeys)
+                if (value == IsEnableHotKeys)
                     return;
-                isEnableHotKeys = value;
+                Settings.Default.IsEnableHotKeys = value;
                 NotifyPropertyChanged();
             }
         }
 
-        [SettingsProperty]
         public bool IsEnableDownloadsWatch
         {
-            get { return isEnableDownloadsWatch; }
+            get { return Settings.Default.IsEnableDownloadsWatch; }
             set
             {
-                if (value == isEnableDownloadsWatch)
+                if (value == IsEnableDownloadsWatch)
                     return;
-                isEnableDownloadsWatch = value;
+                Settings.Default.IsEnableDownloadsWatch = value;
                 NotifyPropertyChanged();
             }
         }
@@ -60,42 +53,12 @@ namespace MMK.Notify.ViewModels
 
         protected override void OnLoadData()
         {
-            LoadSettings();
-        }
-
-        private void LoadSettings()
-        {
-            IsEnableHotKeys = Settings.Default.IsEnableHotKeys;
-            IsEnableDownloadsWatch = Settings.Default.IsEnableDownloadsWatch;
-
             if (IsEnableHotKeys)
                 StartListenShortcutsCommand.Execute(null);
 
             if (IsEnableDownloadsWatch)
                 StartDownloadsWatchCommand.Execute(null);
         }
-
-
-        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            var propertyName = e.PropertyName;
-
-            if (IsSettingsProperty(propertyName))
-                SaveSettings();
-        }
-
-        private bool IsSettingsProperty(string propertyName)
-        {
-            return Attribute.IsDefined(GetType().GetProperty(propertyName), typeof (SettingsPropertyAttribute));
-        }
-
-        private void SaveSettings()
-        {
-            Settings.Default.IsEnableHotKeys = IsEnableHotKeys;
-            Settings.Default.IsEnableDownloadsWatch = IsEnableDownloadsWatch;
-            Settings.Default.Save();
-        }
-
 
         public ICommand StartListenShortcutsCommand { get; set; }
         public ICommand StopListenShortcutsCommand { get; set; }
