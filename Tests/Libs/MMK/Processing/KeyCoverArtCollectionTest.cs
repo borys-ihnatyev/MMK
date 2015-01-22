b98b6931f993d6149e8180919f68dff8245c8ad9
Boris <paur.link@gmail.com>
2015-01-22 07:20:09 +0200

    Add KeyCoverArtCollectionTest

---
+++ Tests/Libs/MMK/Processing/KeyCoverArtCollectionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MMK.Processing
+{
+    [TestFixture]
+    public class KeyCoverArtCollectionTest
+    {
+        private KeyCoverArtCollection keyCoverArtCollection;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            keyCoverArtCollection = new KeyCoverArtCollection("__KeyColorsTest");
+        }
+
+        [Test, TestCaseSource("GetAllKeys")]
+        public void TestCreateImage(Key key)
+        {
+            keyCoverArtCollection.RetriveImagePath(key);
+        }
+
+        public IEnumerable<Key> GetAllKeys()
+        {
+            return CircleOfFifths.AllKeys;
+        }
+    }
+}
