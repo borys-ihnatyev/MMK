4c0cf95d9ab06a53995fc68cb5ffeaf1ed7cfd6c
Boris <paur.link@gmail.com>
2015-01-22 07:18:30 +0200

    Create Dynamic creation of Key-based CoverArt

--- Src/Libs/Core/MMK/Processing/KeyColorConverter.cs
+++ Src/Libs/Core/MMK/Processing/KeyColorConverter.cs
@@ -7,27 +7,16 @@ namespace MMK.Processing
         private static ColorHsvModel ToHsvColorModel(Key key)
         {
             var color = new ColorHsvModel {H = 0.0, S = 1.0, V = 1.0};
-            const double hStep = 1.0/12;
+            const double hStep = ColorHsvModel.HueMax/NoteFactory.NotesCount;
 
-            var keyToneEnumer = CircleOfFifths.MinorKeys.GetEnumerator();
-
-            if (key.IsMoll())
-            {
-                color.S = 0.9;
-                color.V = 0.9;
-            }
-            else
-            {
-                color.S = 1.0;
-                color.V = 1.0;
-                keyToneEnumer = CircleOfFifths.MajorKeys.GetEnumerator();
-            }
+            var keyToneEnumer = (key.IsMoll() ? CircleOfFifths.MinorKeys : CircleOfFifths.MajorKeys).GetEnumerator();
 
             while (keyToneEnumer.MoveNext())
             {
-                color.H += hStep;
                 if (keyToneEnumer.Current == key)
                     break;
+                color.H += hStep;
+
             }
 
             return color;
---
+++ Src/Libs/Core/MMK/Processing/KeyCoverArtCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace MMK.Processing
+{
+    public class KeyCoverArtCollection
+    {
+        private readonly DirectoryInfo coverArtsDir;
+        
+        public KeyCoverArtCollection(string directory)
+        {
+            if(String.IsNullOrWhiteSpace(directory))
+                throw new ArgumentException("invalid directory name","directory");
+            coverArtsDir = new DirectoryInfo(directory);
+
+            if (coverArtsDir.Exists) return;
+            
+            coverArtsDir.Create();
+            coverArtsDir.Refresh();
+        }
+
+        /// <summary>
+        /// returns path to coverart for specific key,
+        /// if not founded it creates new image and saves it
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string RetriveImagePath(Key key)
+        {
+            var imagePath = GetImagePath(key);
+            if (!File.Exists(imagePath))
+                CreateImage(key, imagePath);
+            return imagePath;
+        }
+
+        private string GetImagePath(Key key)
+        {
+            return Path.Combine(coverArtsDir.FullName, String.Format(@"{0}.png", key.ToString(KeyNotation.CamelotWithoutTone)));
+        }
+
+        private static void CreateImage(Key key, string imagePath)
+        {
+            var image = new Bitmap(500, 500);
+            var g = Graphics.FromImage(image);
+            g.FillRectangle(Brushes.Transparent, 0, 0, 500, 500);
+            var keyBrush = CreateKeyBrush(key);
+            g.FillEllipse(keyBrush, 0, 0, 500, 500);
+            image.Save(imagePath, ImageFormat.Png);
+        }
+
+        private static Brush CreateKeyBrush(Key key)
+        {
+            var color = KeyColorConverter.ToArgbByteModel(key);
+            return new SolidBrush(Color.FromArgb(color.Argb));
+        }
+    }
+}
