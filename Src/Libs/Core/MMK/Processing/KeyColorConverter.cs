61a29e4ee41e0109e2750d148fb5b2f53288c05d
Boris <paur.link@gmail.com>
2015-01-22 06:05:34 +0200

    Move color converting logic in KeyColorConverter class

---
+++ Src/Libs/Core/MMK/Processing/KeyColorConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using MMK.Utils.Media;
+
+namespace MMK.Processing
+{
+    public static class KeyColorConverter
+    {
+        private static ColorHsvModel ToHsvColorModel(Key key)
+        {
+            var color = new ColorHsvModel { H = 0.0, S = 1.0, V = 1.0 };
+            const double hStep = 1.0 / 12;
+
+            var keyToneEnumer = CircleOfFifths.MinorKeys.GetEnumerator();
+
+            if (key.IsMoll())
+            {
+                color.S = 0.9;
+                color.V = 0.9;
+            }
+            else
+            {
+                color.S = 1.0;
+                color.V = 1.0;
+                keyToneEnumer = CircleOfFifths.MajorKeys.GetEnumerator();
+            }
+
+            while (keyToneEnumer.MoveNext())
+            {
+                color.H += hStep;
+                if (keyToneEnumer.Current == key)
+                    break;
+            }
+
+            return color;
+        }
+
+        public static ColorArgbDoubleModel ToArgbDoubleModel(Key key)
+        {
+            return ToHsvColorModel(key).ToArgbDoubleModel();
+        }
+
+        public static ColorArgbByteModel ToArgbByteModel(Key key)
+        {
+            return ToHsvColorModel(key).ToArgbByteModel();
+        }
+    }
+}
--- Src/Libs/Presentation/Tools/KeyBrushConverter.cs
+++ Src/Libs/Presentation/Tools/KeyBrushConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 using MMK.Marking;
-using MMK.Presentation.Windows.Media;
+using MMK.Processing;
 
 namespace MMK.Presentation.Tools
 {
@@ -14,48 +14,30 @@ namespace MMK.Presentation.Tools
             DefaultBrush = Brushes.Transparent;
         }
 
-        public Brush DefaultBrush
-        {
-            get; set;
-        }
+        public Brush DefaultBrush { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var keyHashTag = value as KeyHashTag;
-            
+
             if (keyHashTag == null)
                 return DefaultBrush;
 
             return Convert(keyHashTag.Key);
         }
 
-        public static SolidColorBrush Convert(Key key) 
+        public static SolidColorBrush Convert(Key key)
         {
-            var color = new ColorHsv { H = 0.0, S = 1.0, V = 1.0 };
-            const double hStep = 1.0 / 12;
+            var color = KeyColorConverter.ToArgbDoubleModel(key);
 
-            var keyToneEnumer = CircleOfFifths.MinorKeys.GetEnumerator();
-
-            if (key.IsMoll())
-            {
-                color.S = 0.9;
-                color.V = 0.9;
-            }
-            else
-            {
-                color.S = 1.0;
-                color.V = 1.0;
-                keyToneEnumer = CircleOfFifths.MajorKeys.GetEnumerator();
-            }
-
-            while (keyToneEnumer.MoveNext())
-            {
-                color.H += hStep;
-                if (keyToneEnumer.Current == key)
-                    break;
-            }
-
-            return new SolidColorBrush(color);
+            return new SolidColorBrush(
+                Color.FromScRgb(
+                    (float) color.A,
+                    (float) color.R,
+                    (float) color.G,
+                    (float) color.B
+                    )
+                );
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
