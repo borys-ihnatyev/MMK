b76d2060503894cce5f45d30b843952f624ea80f
Boris <paur.link@gmail.com>
2015-01-22 04:27:55 +0200

    Add PresentationStyles project

---
+++ Src/Libs/Core/PresentationStyles/Styles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace MMK.PresentationStyles
+{
+    public sealed class Styles : MarkupExtension
+    {
+        private static readonly ResourceDictionary ResourceDictionary;
+
+        static Styles()
+        {
+            ResourceDictionary = new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/MMK.PresentationStyles;component/Styles.xaml")
+            };
+        }
+
+        public Styles(string resourceKey)
+        {
+            ResourceKey = resourceKey;
+        }
+
+        public Styles()
+        {
+
+        }
+
+        public string ResourceKey { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ResourceDictionary[ResourceKey];
+        }
+
+        public static TResource Get<TResource>(string resourceKey)
+        {
+            return (TResource)ResourceDictionary[resourceKey];            
+        }
+    }
+}
