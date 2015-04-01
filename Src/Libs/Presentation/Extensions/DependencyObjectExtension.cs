8dcde084f44ab710f2360edfce7a205162a9ffe4
Borys Ihnatyev <paur.link@gmail.com>
2015-04-01 13:07:09 +0300

    add ExtensionClass For DependencyObject class

---
+++ Src/Libs/Presentation/Extensions/DependencyObjectExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MMK.Presentation.Extensions
+{
+    public static class DependencyObjectExtension
+    {
+        public static IEnumerable<T> FindVisualChildren<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield break;
+            
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                var children = child as T;
+                if (children != null)
+                    yield return children;
+
+                foreach (var childOfChild in FindVisualChildren<T>(child))
+                    yield return childOfChild;
+            }
+        }
+    }
+}
